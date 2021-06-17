ce56751669fb844931e4a597a162e1c7629febbe
溝手彩加 <>
2021-06-17 18:11:53 +0900

    β 改

---
+++ Assets/Scripts/RetryController_1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryController_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RetryButton1()
+    {
+        SceneManager.LoadScene("World_1");
+    }
+}
